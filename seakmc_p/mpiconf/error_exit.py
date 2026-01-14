9232e7ebccfd2a15477306b5440bb220d5d20b52
Tao Liang <xhtliang120@gmail.com>
2026-01-14 16:46:43 -0500

    Update error_exit.py

--- seakmc_p/mpiconf/error_exit.py
+++ seakmc_p/mpiconf/error_exit.py
@@ -4,7 +4,7 @@ rank_world = MPI.COMM_WORLD.Get_rank()
 def error_exit(error_str):
     MPI.Finalize()
     if rank_world == 0:
-        print("=== Error Message===")
+        print("=== Error Message ===")
         print(error_str)
         print("=== End of Error ===")
     sys.exit(1)

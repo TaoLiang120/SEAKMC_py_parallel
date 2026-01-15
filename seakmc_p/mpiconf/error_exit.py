76786bf58a09792da0be836cf90a3ee40c17337c
Tao Liang <xhtliang120@gmail.com>
2026-01-15 10:22:05 -0500

    Add files via upload

--- seakmc_p/mpiconf/error_exit.py
+++ seakmc_p/mpiconf/error_exit.py
@@ -1,10 +1,10 @@
 import sys
 from mpi4py import MPI
-rank_world = MPI.COMM_WORLD.Get_rank()
 def error_exit(error_str):
-    MPI.Finalize()
-    if rank_world == 0:
-        print("=== Error Message ===")
-        print(error_str)
-        print("=== End of Error ===")
-    sys.exit(1)
+    print("=== Error Message ===")
+    print(error_str)
+    print("=== End of Error ===")
+    sys.stdout.flush()
+    MPI.COMM_WORLD.Abort(1)
+    #MPI.Finalize()
+    #sys.exit(0)
